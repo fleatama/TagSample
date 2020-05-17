6d12c9c8c5d36d692faa1936d10f571838d519a2
fleatama <fleatama@gmail.com>
2020-05-17 16:17:23 +0900

    change posts _form.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+##省略
+#ここから追記
+Tag.create([
+    { name: 'スターバックス' },
+    { name: 'ドトール' },
+    { name: 'エクセルシオール'},
+    { name: 'タリーズ'},
+    { name: 'サンマルク'},
+    { name: 'コメダ'}
+    ])
+#ここまで
